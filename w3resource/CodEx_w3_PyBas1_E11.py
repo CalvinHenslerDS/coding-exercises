8bc3d00b443e693c212d32c300204556a748479b
CalvinHenslerDS <CalvinHenslerDS@gmail.com>
2025-11-09 22:01:33 -0500

    Two Python exercises

---
+++ w3resource/CodEx_w3_PyBas1_E11.py
@@ -0,0 +1,26 @@
+# w3resource
+
+# Python Basic Part -I
+
+
+# Exercise 11:
+
+# Problem Statement:
+
+# Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).
+# Sample function : abs()
+# Expected Result :
+# abs(number) -> number
+# Return the absolute value of the argument.
+
+
+# Solution Attempt:
+
+# Print the docstring of a built-in function
+print(abs.__doc__)
+
+
+# Lessons Learned:
+
+# Syntax:
+# __doc__: Appending to a built-in functino or object retrieves and prints its associated docstring
---
+++ w3resource/CodEx_w3_PyBas1_E12.py
@@ -0,0 +1,33 @@
+# w3resource
+
+# Python Basic Part -I
+
+
+# Exercise 11:
+
+# Problem Statement:
+
+# Write a Python program that prints the calendar for a given month and year.
+# Note : Use 'calendar' module.
+
+
+# Solution Attempt:
+
+# Import the calendar module
+import calendar
+
+# Prompt the user to input a year and month
+year = int(input("Input a year (####): "))
+month = int(input("Input a month (##): "))
+
+# Print the calendar for the specified year and month
+print(calendar.month(year,month))
+
+
+# Lessons Learned:
+
+# Modules:
+# calendar: Provides functions and classes for working with calendars
+
+# Syntax:
+# calendar.month(): Retrieves the calendar for a given year and month using the calendar module
